6827e8b1515b30b6f4aabe45d2e4ce789951eb02
XiterBug <anakselatan34@gmail.com>
2026-07-08 06:28:55 +0700

    Hapus fix-app3.cjs

--- fix-app3.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/App.tsx', 'utf8');
-code = code.replace(/          <\/div>\n          \)}\n          \)}\n        <\/div>/, 
-"          </div>\n          )}\n        </div>");
-fs.writeFileSync('src/App.tsx', code);
