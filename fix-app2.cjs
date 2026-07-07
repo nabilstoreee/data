ad0c57ec52cfc5e9d2060bb17511413dfe7e931f
XiterBug <anakselatan34@gmail.com>
2026-07-08 06:28:50 +0700

    Hapus fix-app2.cjs

--- fix-app2.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/App.tsx', 'utf8');
-code = code.replace(/\{user\?\.email === 'jrnabil570@gmail\.com' && \(\n          \{user\?\.email === 'jrnabil570@gmail\.com' && \(\n/, "{user?.email === 'jrnabil570@gmail.com' && (\n");
-fs.writeFileSync('src/App.tsx', code);
